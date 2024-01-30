3490f83eac73741137f806e480f59a396943fbdf
shubham0204 <equipintelligence@gmail.com>
2024-01-30 19:26:27 +0530

    [update] fixed compiler warnings, [new] README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# Product Quantization in Rust
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::vec;
+use num_traits::ToPrimitive;
 use num_traits::sign::Unsigned;
 use num_traits::cast::FromPrimitive;
 use rand::seq::SliceRandom; 
@@ -23,15 +24,20 @@ impl ProductQuantizer {
         n_codes: usize ,
         src_vec_dims: usize ,
         metric: DistanceMetric
-        ) -> Self {
+    ) -> Self {
         ProductQuantizer{ n_subvectors , n_codes , src_vec_dims , codewords: Vec::new() , metric }
     }
 
+    /// Given the vectors, derive the centroids which can be used to quantize vectors using
+    /// `encode`.
+    /// # Arguments
+    /// * `src_vectors` - Vectors from which centroids would be derived
+    /// * `iterations` - Number of iterations to determine centroids with K-means
     pub fn fit(
         self: &mut ProductQuantizer , 
         src_vectors: &Vec<Vec<f32>> , 
         iterations: usize
-        ) {
+    ) {
         // `sub_vec_dims` is the dimensionality of the subvector
         // A subvector is a part of the source vector, obtained by dividing the source 
         // vector equally into `n_subvectors` parts
@@ -51,7 +57,7 @@ impl ProductQuantizer {
             // Perform K means clustering on `sub_vectors_m`
             // with K = `n_codes` and data points `sub_vectors_m`
             // It returns centroid having dims ( n_codes , sub_vec_dims )
-            self.codewords.push( self.kmeans( &sub_vectors_m , self.n_codes , iterations ) ) ; 
+            self.codewords.push( ProductQuantizer::kmeans( &sub_vectors_m , self.n_codes , iterations ) ) ; 
         }
     }
 
@@ -69,46 +75,31 @@ impl ProductQuantizer {
         for vec in vectors {
             let mut subvectors: Vec<Vec<f32>> = Vec::new() ; 
             for m in 0..self.n_subvectors {
-                subvectors.push( vec[  (m * sub_vec_dims)..((m+1) * sub_vec_dims) ].to_vec() )
+                subvectors.push( vec[  (m * sub_vec_dims)..((m+1) * sub_vec_dims) ].to_vec() ) ; 
             }
             vector_codes.push( self.vector_quantize( &subvectors ) ) ; 
         }
         vector_codes
     }
 
-    pub fn dtable(
+    pub fn search<T>( 
         self: &ProductQuantizer , 
-        query: &Vec<f32>
-        ) -> Vec<Vec<f32>> {
-        let sub_vec_dims: usize = self.src_vec_dims / self.n_subvectors ; 
-        let mut dtable: Vec<Vec<f32>> = Vec::new() ; 
-        for m in 0..self.n_subvectors {
-            let query_sub: Vec<f32> = query[ (m * sub_vec_dims)..((m+1) * sub_vec_dims) ].to_vec() ; 
-            let mut m_sub_distances: Vec<f32> = Vec::new() ; 
-            for k in 0..self.n_codes {
-                m_sub_distances.push( self.euclid_distance( &self.codewords[m][k] , &query_sub ) )
-            }
-            dtable.push( m_sub_distances ) ; 
-        }
-        dtable
-    }
-
-    pub fn search( 
-        self: &ProductQuantizer , 
-        queries: &Vec<Vec<f32>> ,
-        codes: &Vec<Vec<u32>> , 
-        k: usize
-        ) -> Vec<usize> {
+        queries: &[Vec<f32>] ,
+        codes: &[Vec<T>] 
+    ) -> Vec<usize> where T: Unsigned + FromPrimitive + ToPrimitive {
         let sub_vec_dims: usize = self.src_vec_dims / self.n_subvectors ; 
         let mut distances: Vec<usize> = Vec::new() ; 
-        for query in queries.iter() {
+        for query in queries {
             let mut min_distance = f32::MAX ;
             let mut min_distance_index = 0 ; 
-            for n in 0..codes.len() {
+            for ( n , code ) in codes.iter().enumerate() {
                 let mut distance = 0.0 ; 
                 for m in 0..self.n_subvectors {
-                    let query_sub: Vec<f32> = query[ m * sub_vec_dims..((m+1) * sub_vec_dims) ].to_vec() ; 
-                    distance += self.euclid_distance( &query_sub, &self.codewords[m][ codes[n][m] as usize ] ) ;
+                    let query_sub: Vec<f32> = query[ m * sub_vec_dims..((m+1) * sub_vec_dims) ].to_vec() ;
+
+                    // Computing asymmetric distance between the subvector of the query
+                    // and the quantized subvector 
+                    distance += ProductQuantizer::euclid_distance( &query_sub, &self.codewords[m][ code[m].to_usize().unwrap() ] ) ;
                 }
                 if  min_distance > distance {
                     min_distance = distance ;
@@ -125,15 +116,15 @@ impl ProductQuantizer {
     /// which each vector is the nearest
     fn vector_quantize<T>(
         self: &ProductQuantizer , 
-        vector: &Vec<Vec<f32>> 
-        ) -> Vec<T> where T: FromPrimitive + Unsigned {
+        vector: &[Vec<f32>] 
+    ) -> Vec<T> where T: FromPrimitive + Unsigned {
 
         let mut codes: Vec<T> = Vec::new() ; 
         for ( m , subvector ) in vector.iter().enumerate() {
             let mut min_distance: f32 = f32::MAX ; 
             let mut min_distance_code_index: T = T::from_u8( 0 ).unwrap() ; 
             for ( k , code ) in self.codewords[m].iter().enumerate() {
-                let distance = self.euclid_distance( subvector , code ) ; 
+                let distance = ProductQuantizer::euclid_distance( subvector , code ) ; 
                 if distance < min_distance {
                     min_distance = distance ; 
                     min_distance_code_index = T::from_usize( k ).unwrap(); 
@@ -149,16 +140,15 @@ impl ProductQuantizer {
     /// which represent the centroids of the clusters
     /// derived using the standard algorithm of K-means clusterings
     fn kmeans(
-        self: &ProductQuantizer , 
         vecs: &Vec<Vec<f32>> , 
         n_clusters: usize , 
         iter: usize
-        ) -> Vec<Vec<f32>> {
+    ) -> Vec<Vec<f32>> {
         let mut assigned_centroids: Vec<Vec<f32>> = vec![ Vec::new() ; vecs.len() ] ; 
 
         // Choose random samples from `vecs` as initial centroids
         let mut centroids: Vec<Vec<f32>> = vecs.choose_multiple( &mut rand::thread_rng() , n_clusters )  
-            .map( | vec | vec.to_vec() )
+            .cloned()
             .collect() ; 
 
         let vec_dims: usize = vecs[0].len() ; 
@@ -176,7 +166,7 @@ impl ProductQuantizer {
                 let mut min_centroid: Vec<f32> = centroids[0].clone() ; 
                 for centroid in &centroids {
                     // Calculate distance between ith vector and `centroid`
-                    let distance: f32 = self.euclid_distance( &vecs[i] , centroid ) ;
+                    let distance: f32 = ProductQuantizer::euclid_distance( &vecs[i] , centroid ) ;
                     if distance < min_centroid_distance {
                         min_centroid_distance = distance ; 
                         min_centroid = centroid.clone() ; 
@@ -197,15 +187,15 @@ impl ProductQuantizer {
                 // nearest_centroid = assigned_centroids[i]
                 for j in 0..assigned_centroids.len() {
                     if assigned_centroids[ j ] == centroids[ i ] {
-                        self.vec_add( &mut vec_sum , &vecs[j] ) ; 
+                        ProductQuantizer::vec_add( &mut vec_sum , &vecs[j] ) ; 
                         count += 1 ; 
                     }
                 }
-                self.vec_scale( &mut vec_sum , 1.0 / (count as f32) ) ; 
+                ProductQuantizer::vec_scale( &mut vec_sum , 1.0 / (count as f32) ) ; 
 
                 // Update the centroid with 
                 // the average of the vectors
-                centroids[ i ] = Vec::from( vec_sum ) ; 
+                centroids[ i ] = vec_sum ; 
             }
         }    
 
@@ -215,33 +205,30 @@ impl ProductQuantizer {
     /// For given vector `vec`,
     /// multiply each element of `vec` with `scale`, inplace
     fn vec_scale(
-        self: &ProductQuantizer , 
         vec: &mut Vec<f32> , 
         scale: f32
-        ) {
-        for i in 0..vec.len() {
-            vec[ i ] = vec[ i ] * scale ; 
+    ) {
+        for element in vec.iter_mut() {
+            *element *= scale ; 
         }
     }
 
     /// Add `vec1` and `vec2` elementwise
     /// and store the result in `vec1` (through a mutable reference)
     fn vec_add(
-        self: &ProductQuantizer , 
         vec1: &mut Vec<f32> , 
-        vec2: &Vec<f32>
-        ) {
+        vec2: &[f32]
+    ) {
         for i in 0..vec1.len() {
-            vec1[ i ] = vec1[ i ] + vec2[ i ] ; 
+            vec1[ i ] += vec2[ i ] ; 
         }
     }
 
     /// Compute the Euclidean distance between vectors `vec1` and `vec2`
     fn euclid_distance(
-        self: &ProductQuantizer ,
-        vec1: &Vec<f32> , 
-        vec2: &Vec<f32>
-        ) -> f32 {
+        vec1: &[f32] , 
+        vec2: &[f32]
+    ) -> f32 {
         let mut squared_diff_sum = 0.0 ; 
         for i in 0..vec1.len() {
             squared_diff_sum += ( vec1[i] - vec2[i] ).powi( 2 ) ; 
@@ -250,10 +237,9 @@ impl ProductQuantizer {
     }
 
     fn dot( 
-        self: &ProductQuantizer , 
-        vec1: &Vec<f32> , 
-        vec2: &Vec<f32>
-        ) -> f32 {
+        vec1: &[f32] , 
+        vec2: &[f32]
+    ) -> f32 {
         let mut dot_product = 0.0 ; 
         for i in 0..vec1.len() {
             dot_product += vec1[i] * vec2[i] ; 
@@ -284,9 +270,7 @@ fn main() {
         &vectors , 100
         ) ;
     let vector_codes = quantizer.encode::<u32>( &vectors ) ;
-    let dtable = quantizer.dtable( &vectors[4] ) ;
-    let distances = quantizer.search( &vectors , &vector_codes , 2) ;
+    let distances = quantizer.search::<u32>( &vectors , &vector_codes ) ;
     println!( "{:?}" , distances ) ; 
     println!( "{:?}" , vector_codes ) ;
-    println!( "{:?}" , dtable ) ; 
 }
